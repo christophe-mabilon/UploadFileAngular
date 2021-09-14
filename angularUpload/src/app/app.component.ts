4d9312f846e7a5172828ed431573b0913c815407
Mabilon Christophe <70352608+christophe-mabilon@users.noreply.github.com>
2021-09-14 12:59:31 +0200

    app fonctionelle

---
+++ angularUpload/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { FileService } from './file.service';
+import { Component, ViewChild, ElementRef} from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  public over: boolean;
+  public filesHolder$: Observable<
+    { file: File; progress$: Observable<number> }[]
+  > = this.fileService.filesHolder$.asObservable();
+  @ViewChild('fileinput', { static: true }) inputRef: ElementRef;
+
+  constructor(private fileService: FileService) {}
+
+  openFile() {
+    this.inputRef.nativeElement.click();
+  }
+
+  addFiles($event) {
+    const files = $event.target.files;
+    this.fileService.addFiles(files);
+  }
+
+  removeFile(index) {
+    this.fileService.removeFile(index);
+  }
+
+  dropFile($event) {
+    if ($event.dataTransfer) {
+      const files = $event.dataTransfer.files;
+      this.fileService.addFiles(files);
+    }
+  }
+}
---
+++ angularUpload/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
